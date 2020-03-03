f6bf009be7f442ff5f6e8300268ba0ecc8ee7632
HaroldOpara <haroldopara7@gmail.com>
2020-03-03 18:26:48 +0000

    UI menu scene, and UI resource counter

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class ControllerScript : MonoBehaviour
 {
+    public Text rockValue;
+    public Text woodValue;
+    public Text goldValue;
+    
     private int rockOwned;
     private int woodOwned;
     private int goldOwned;
@@ -12,17 +17,30 @@ public class ControllerScript : MonoBehaviour
         rockOwned = 0;
         woodOwned = 0;
         goldOwned = 0;
-    }
-    
-    public void AddRock(int n){
-        rockOwned += n;
+
+        rockValue.text = "= " + rockOwned.ToString ();
+        woodValue.text = "= " + woodOwned.ToString ();
+        goldValue.text = "= " + goldOwned.ToString ();
+
     }
 
-    public void AddWood(int n){
-        woodOwned += n;
+    public void AddRock(int n)
+    {
+        //Debug.Log("ROCK ADDED");
+        rockOwned = rockOwned + 1;
+        rockValue.text = "= " + rockOwned.ToString ();
+
+    }
+
+    public void AddWood(int n)
+    {
+        woodOwned = woodOwned + 1;
+        woodValue.text = "= " + woodOwned.ToString ();
+
     }
 
     public void AddGold(int n){
-        goldOwned += n;
+        goldOwned = goldOwned + 1;
+        goldValue.text = "= " + goldOwned.ToString ();
     }
 }
