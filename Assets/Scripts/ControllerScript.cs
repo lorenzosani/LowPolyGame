1abebe3f242a70ff8caa662e12738683331cc559
lorexsan <lorenzosani97@gmail.com>
2020-03-13 17:46:09 +0000

    Improve ships attacks

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -2,8 +2,6 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 
 public class ControllerScript : MonoBehaviour
 {
@@ -22,12 +20,6 @@ public class ControllerScript : MonoBehaviour
     public GameObject housePrefab;
     private Vector3 houseInitialPosition;
     private bool placing;
-
-    private bool attacked = false;
-    private bool attacking = false;
-    public GameObject attackingShip;
-    public GameObject attackingDialog;
-    public GameObject attackResultDialog;
 
     void Start(){
         rockOwned = 0;
@@ -56,38 +48,13 @@ public class ControllerScript : MonoBehaviour
                 }
             }
         }
-
-        if (woodOwned >= 5 & !attacked) {
-            attackingShip.gameObject.SetActive(true);
-            attackingDialog.gameObject.SetActive(true);
-            attacked = true;
-            attacking = true;
-        }
-        if (attacking & Input.GetMouseButton(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.gameObject.layer == 12)
-                {
-                    attackResultDialog.transform.GetChild(0).GetComponent<Text>().text = generateAttackOutcome();
-                    attackResultDialog.gameObject.SetActive(true);
-                    attackingDialog.gameObject.SetActive(false);
-                    attackingShip.gameObject.SetActive(false);
-
-                }
-            }
-        }
         rockValue.text = "= " + rockOwned.ToString ();
         woodValue.text = "= " + woodOwned.ToString ();
         goldValue.text = "= " + goldOwned.ToString ();
-
     }
 
     public void AddRock(int n)
     {
-        //Debug.Log("ROCK ADDED");
         rockOwned = rockOwned + 1;
         rockValue.text = "= " + rockOwned.ToString ();
 
@@ -120,17 +87,5 @@ public class ControllerScript : MonoBehaviour
             placing = true;
             woodOwned -= 4;
         }
-    }
-
-    private string generateAttackOutcome() {
-        int rockStolen = (int) UnityEngine.Random.Range(1.0F, (float) rockOwned);
-        int woodStolen = (int) UnityEngine.Random.Range(1.0F, (float) woodOwned);
-        int goldStolen = (int) UnityEngine.Random.Range(1.0F, (float) goldOwned);
-
-        rockOwned -= rockStolen;
-        woodOwned -= woodStolen;
-        goldOwned -= goldStolen;
-
-        return string.Format("Your village has been attacked by pirates! You lost {0} rock, {1} wood, {2} gold.", rockStolen, woodStolen, goldStolen);
     }
 }
---
+++ Assets/Scripts/shipAttack.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipAttack : MonoBehaviour
+{   
+    public float attackFrequencyInSeconds = 150.0f;
+    public GameObject[] ships = new GameObject[3];
+    public Vector3 attackingShipPosition;
+    public GameObject attackingDialog;
+    public GameObject attackResultDialog;
+
+    void Start()
+    {
+        float nextAttackTime = Random.Range(30, 30+attackFrequencyInSeconds*2);
+        Invoke("generateAttack", nextAttackTime);
+    }
+
+    void LateUpdate()
+    {
+    // TODO:
+    //1. CLICK ON THE SHIP HIDES THE WARNING
+	//2. CLICK ON THAT SHIP WILL PROVIDE YOU INFORMATION ABOUT WHO WON THE FIGHT
+	//3. CLICK MAKES THE SHIP GO AWAY
+
+/*
+        if (woodOwned >= 5 & !attacked) {
+            attackingShip.gameObject.SetActive(true);
+            attackingDialog.gameObject.SetActive(true);
+            attacked = true;
+            attacking = true;
+        }
+        if (attacking & Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.layer == 12)
+                {
+                    attackResultDialog.transform.GetChild(0).GetComponent<Text>().text = generateAttackOutcome();
+                    attackResultDialog.gameObject.SetActive(true);
+                    attackingDialog.gameObject.SetActive(false);
+                    attackingShip.gameObject.SetActive(false);
+
+                }
+            }
+        }
+*/
+    }
+
+    private void generateAttack()
+    {
+        // Generate random attacking ship among available ships
+        int shipSize = Random.Range(0,ships.Length-1);
+        GameObject ship = ships[shipSize];
+
+        // Spawn attacking ship at given position
+        Instantiate(ship, attackingShipPosition, new Quaternion(0,0,0,1));
+
+        // Warn player that the village is under attack
+        attackingDialog.gameObject.SetActive(true);
+
+        float nextAttackTime = Random.Range(30, 30+attackFrequencyInSeconds*2);
+        Invoke("generateAttack", nextAttackTime);
+    }
+
+/*
+    private string generateAttackOutcome() {
+        int rockStolen = (int) UnityEngine.Random.Range(1.0F, (float) rockOwned);
+        int woodStolen = (int) UnityEngine.Random.Range(1.0F, (float) woodOwned);
+        int goldStolen = (int) UnityEngine.Random.Range(1.0F, (float) goldOwned);
+
+        rockOwned -= rockStolen;
+        woodOwned -= woodStolen;
+        goldOwned -= goldStolen;
+
+        return string.Format("Your village has been attacked by pirates! You lost {0} rock, {1} wood, {2} gold.", rockStolen, woodStolen, goldStolen);
+    }
+*/
+}
