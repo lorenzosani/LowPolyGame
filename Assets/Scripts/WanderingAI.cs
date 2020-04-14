7c8b8e26b30a60a27c6175a513f98956c34c33f3
lorexsan <lorenzosani97@gmail.com>
2020-04-14 22:47:33 +0100

    Animate characters

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
  
 public class WanderingAI : MonoBehaviour {
  
     public float wanderRadius;
     public float wanderTimer;
- 
+    public ThirdPersonCharacter character;
+
+    private Animator animator;
     private Transform target;
     private NavMeshAgent agent;
+    private Rigidbody rigidbody;
     private float timer;
+    private Vector3 lastPosition;
  
-    void OnEnable () {
+    void Start() {
+        lastPosition = transform.position;
         agent = GetComponent<NavMeshAgent> ();
+        agent.updateRotation = false;
+        animator = GetComponent<Animator>();
         timer = wanderTimer;
     }
  
@@ -22,6 +31,11 @@ public class WanderingAI : MonoBehaviour {
             Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
             agent.SetDestination(newPos);
             timer = 0;
+        }
+        if (lastPosition != transform.position) {
+            character.Move(agent.desiredVelocity, false, false);
+        } else {
+            character.Move(Vector3.zero, false, false);
         }
     }
  
